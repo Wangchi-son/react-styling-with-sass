1c8f83fa59f208147690dd7f8069d1cde27a15e0
unknown <sonwj0915@naver.com>
2021-04-08 21:05:00 +0900

    sass 버튼 size props 설정

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,18 @@
-import React from "react";
-import "./Button.scss";
+import React from 'react';
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+import classNames from 'classnames';
+import './Button.scss';
+
+function Button({ children, size }) {
+  return (
+    <>
+      <button className={classNames('Button', size)}>{children}</button>
+    </>
+  );
 }
+
+Button.defaultProps = {
+  size: 'medium',
+};
 
 export default Button;
